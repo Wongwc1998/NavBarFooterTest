9bad191b078b87a838d5dc3442b40ea387f2c4e3
Wongwc1998 <wongwc1998@gmail.com>
2023-09-16 20:08:25 +0800

    escape apos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,14 @@ export default function Home() {
     <main className="flex flex-col items-center justify-between p-24">
       <section className="max-w-4xl bg-yellow-300 p-10 rounded-xl flex flex-col items-center gap-10">
         <div className="bg-yellow-500 text-center max-w-fit py-5 px-10 rounded-xl">
-          <h1 className="text-4xl">Beary's Breakfast Bar</h1>
+          <h1 className="text-4xl">Beary&apos;s Breakfast Bar</h1>
         </div>
         <div className="bg-yellow-500 text-left max-w-fit py-5 px-10 rounded-xl">
           <p className="py-4">
-            Beary's has the best porridge! The atmosphere and customer service
-            make you feel like you are sitting in the middle of the woods,
-            eating like a bear! This is exactly the kind of place that I like to
-            return to again and again.
+            Beary&apos;s has the best porridge! The atmosphere and customer
+            service make you feel like you are sitting in the middle of the
+            woods, eating like a bear! This is exactly the kind of place that I
+            like to return to again and again.
           </p>
           <h3 className="text-xl px-10">Goldilocks</h3>
         </div>
